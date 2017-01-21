6440f4d6e14ad8a03bc176ccedecf3e0289689d0
Alec Kretch <aleckretch@gmail.com>
2017-01-21 05:42:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
   appName: "Venus Dating App",
-  publicServerURL: "http://datesonvenus.com/api,
+  publicServerURL: "http://s.datesonvenus.com/api",
   emailAdapter: new SimpleMailgunAdapter({
     apiKey: 'key-070a23689abf8a03bbacfecad67d8712',
     domain: 'http://mg.datesonvenus.com',
