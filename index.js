c77cb5fc5a6a0ac71ab3852fc5c6a77041b1e139
Alec Kretch <aleckretch@18-3.priv23.nus.edu.sg>
2017-01-21 06:30:08 -0700

    mailgun 2

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ if (!databaseUri) {
 var api = new ParseServer({
   verifyemail: true,
   appName: 'Venus Dating App',
-  publicServerURL: 'http://s.datesonvenus.com/api',
+  publicServerURL: process.env.SERVER_URL,
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   emailAdapter: {
-    module: 'parse-server-mailgun',
+    module: 'parse-server-simple-mailgun-adapter',
     options: {
       fromAddress: 'Venus Dating App Email Verification <verifyemail@datesonvenus.com>',
       domain: 'mg.datesonvenus.com',
